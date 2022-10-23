64a9d5bf583739e6ca8d98136f8fb46aa76da93a
anakkaw <103820046+anakkaw@users.noreply.github.com>
2022-10-23 16:36:57 +0700

    Update iden2.py

--- iden2.py
+++ iden2.py
@@ -9,7 +9,7 @@ st.subheader('Please upload sample_coordinate.csv' )
 uploaded_files = st.file_uploader("Choose a CSV file", type='csv')
 
 df = pd.read_csv(uploaded_files)
-df = df.drop('landmark')
+df = df.drop(columns=['landmark'])
 lm_list = df.values.tolist()
 
 sp = ''
