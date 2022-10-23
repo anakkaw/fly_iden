16ce69d6288aab0b5f8059ffc883435f71815a3b
anakkaw <103820046+anakkaw@users.noreply.github.com>
2022-10-23 16:44:11 +0700

    Update iden2.py

--- iden2.py
+++ iden2.py
@@ -12,8 +12,12 @@ df = pd.read_csv(uploaded_files)
 df = df.drop(columns=['landmark'])
 lm_list = df.values.tolist()
 
+def dist(la, lb):
+    result = ((la[0]-lb[0])**2+(la[1]-lb[1])**2)**(1/2)
+    return result
+Da = dist(lm_list[0],lm_list[1])
 sp = ''
 
 if st.button('Identity'):
      st.write('Your sample is ...')
-     st.subheader(lm_list)
+     st.subheader(Da)
