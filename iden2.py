fb8333f176ed05cfa6eef17b8b09ab3999ce3cf2
anakkaw <103820046+anakkaw@users.noreply.github.com>
2022-10-23 16:31:57 +0700

    Update iden2.py

--- iden2.py
+++ iden2.py
@@ -1,14 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Fly Species Identification Software')
 st.warning('This software can identify CN, CM, CR, LC, HL, MD and PD')
 
-st.subheader('Please input your sample morphological character' )
-
-sp = ''
+st.subheader('Please upload sample_coordinate.csv' )
 
 uploaded_files = st.file_uploader("Choose a CSV file", type='csv')
 
+df = pd.read_csv(uploaded_files)
+lm_list = df.values.tolist()
+
+sp = ''
+
 if st.button('Identity'):
      st.write('Your sample is ...')
-     st.subheader(sp)
+     st.subheader(lm_list)
